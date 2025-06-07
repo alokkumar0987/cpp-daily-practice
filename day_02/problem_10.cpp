9c8971313f139dfb8910989a257aafe8df9b6d95
alokkumar0987 <alok33778@gmail.com>
2025-06-07 22:56:47 +0530

    Swapping two number

---
+++ day_02/problem_10.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"Enter a number:"<<" ";
+    cin>>a;
+    cout<<"Enter a number:"<<" ";
+    cin>>b;
+    cout<<"Befor swapping"<<endl;
+    cout<<"a= "<<a<<endl;
+     cout<<"b= "<<b<<endl;
+    c=b;
+    b=a;
+    a=c;
+    cout<<"After swapping"<<endl;
+     cout<<"a= "<<a<<endl;
+     cout<<"b= "<<b<<endl;
+
+
+}
