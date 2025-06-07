dabd448346e2d9e473768ac00c3c9cf01f1c2120
alokkumar0987 <alok33778@gmail.com>
2025-06-07 22:13:32 +0530

    counting number

---
+++ day_02/problem_07.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,n;
+    cout<<"Enter the number for counting "<<"";
+
+    cin>>n;
+
+    for(i=0; i<=n; i++){
+       cout<<i<<endl;  
+    }
+    
+}
