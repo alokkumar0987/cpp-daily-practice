0f126df075d8e191668fc88c0bff4617dce6b151
alokkumar0987 <alok33778@gmail.com>
2025-06-07 22:21:06 +0530

    Reverse counting printing

---
+++ day_02/problem_08.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,n;
+    cout<<"Enter a number for reverse counting:"<<"";
+    cin>>n;
+    for(i=n;i>=0;i--){
+        cout<<i<<endl;
+
+    }
+}
