4052edeedff1a9060f7fc53cba9c119ac9d9d95a
alokkumar0987 <alok33778@gmail.com>
2025-06-07 22:39:40 +0530

    Check given number is odd or even

---
+++ day_02/problem_09.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number:"<<endl;
+    cin>>n;
+
+    if (n%2==0){
+        cout<<"Entered number is Even"<<endl;
+
+    }
+    else {
+        cout<<"Entered number is odd"<<endl;
+    }
+}
