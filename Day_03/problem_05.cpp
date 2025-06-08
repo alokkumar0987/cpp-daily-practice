a2cd19b6ca0c898c80b29ec85a75fb6481763f1d
alokkumar0987 <alok33778@gmail.com>
2025-06-08 11:43:57 +0530

    uppercase letters in reverse order Z TO A

---
+++ Day_03/problem_05.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"alphabets from Z TO A:";
+    for(ch='Z';ch>= 'A';ch--){
+        cout<<ch<< " ";
+
+    }
+    cout<<endl;
+    return 0;
+}
