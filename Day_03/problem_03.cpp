061eba9443700362214416389efd82d47a01ff9a
alokkumar0987 <alok33778@gmail.com>
2025-06-08 11:24:57 +0530

    check vowel using switch case

---
+++ Day_03/problem_03.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char c;
+    cout<<"Enter a alphabate letter:"<<" ";
+    cin>>c;
+switch (c)
+{
+case 'a':
+cout<<"Vowel"<<endl;
+break;
+case 'A':
+cout<<"Vowel"<<endl;
+break;
+case 'E':
+cout<<"Vowel"<<endl;
+break;
+case 'e':
+cout<<"Vowel"<<endl;
+break;
+case 'I':
+cout<<"Vowel"<<endl;
+break;
+case 'i':
+cout<<"Vowel"<<endl;
+break;
+case 'o':
+cout<<"Vowel"<<endl;
+break;
+case 'O':
+cout<<"Vowel"<<endl;
+break;
+case 'U':
+cout<<"Vowel"<<endl;
+break;
+case 'u':
+cout<<"Vowel"<<endl;
+break;
+default:
+cout<<"consonant"<<endl;
+
+}
+
+}
