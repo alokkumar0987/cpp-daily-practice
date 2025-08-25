78dc8281229e4cfda22e8e7ee756167a64fd0dfe
alokkumar0987 <alok33778@gmail.com>
2025-08-25 19:24:35 +0530

    updating

--- Day_04/problem_02.cpp
+++ Day_04/problem_02.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
-using namespace std;        
+using namespace std;  
+// This program finds the fractional part of a number without using the modulus operator      
 int main() {
    float x;
    cout << "Enter a number: ";
